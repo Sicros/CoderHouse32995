124e3355fce23ccac9c1eff3637c5e91437d40ca
Sicros <francisco.castrou@gmail.com>
2023-01-19 17:27:22 -0300

    Se agregan comentario a los scripts del desafío 06

--- Desafios/D05Camaras/Assets/Scripts/Camera.cs
+++ Desafios/D05Camaras/Assets/Scripts/Camera.cs
@@ -5,16 +5,29 @@ using Cinemachine;
 
 public class Camera : MonoBehaviour
 {
+    // Variable que identifica a la cámara en primera persona.
     [SerializeField] private CinemachineVirtualCamera firstPerson;
+
+    // Variable que identifica a la cámara en tercera persona.
     [SerializeField] private CinemachineVirtualCamera thirdPerson;
+
+    // Botón con que el se alterna entre ambas cámaras.
     [SerializeField] private KeyCode changeCamKey;
+
+    // Identifica si la cámara en primera persona está activa (true) o desactivada (false).
     [SerializeField] private bool activeFirstPerson;
 
+    // Se fija la cámara inicial dependiendo del valor que se le asigne a activeFirstPerson.
+    // Si está activo, se usará la cámara en primera persona, de lo contrario será en
+    // tercera persona.
     void Start()
     {
         activeFirstPerson = ChangeCamera(activeFirstPerson, firstPerson, thirdPerson);
     }
 
+    // Similar a la acción realizar en el método de Start(), solo que esta vez es el usuario
+    // quien debe presionar una tecla para hacer el cambio entre cámaras. La tecla se define
+    // desde el inspector de Unity.
     void Update()
     {
         if (Input.GetKeyDown(changeCamKey))
@@ -23,6 +36,10 @@ public class Camera : MonoBehaviour
         }
     }
 
+    // Método que permite alternar entre dos cámaras. El primer parámetro indica si la cámara
+    // a utilizar es en primera persona (firstPersonCam), si es true se activa esta y se
+    // desactiva la otra (thirdPersonCam), si es false ocurre lo contrario.
+    // El método retorna el valor contrario al booleano ingresado.
     private bool ChangeCamera(bool isFirstPerson, CinemachineVirtualCamera firstPersonCam, CinemachineVirtualCamera thirdPersonCam)
     {
         firstPersonCam.gameObject.SetActive(isFirstPerson);
--- Desafios/D05Camaras/Assets/Scripts/Character.cs
+++ Desafios/D05Camaras/Assets/Scripts/Character.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    // Velocidad a la que se mueve el persona.
     [SerializeField] float speed;
+
+    // Movimiento que realiza el personaje (moviendo en los ejes X,Z)
     [SerializeField] Vector3 movement;
 
+    // Permite usar las teclas AD para moverse hacía los lados (eje X),
+    // y WS para moverse hacía arriba y abajo (eje Z). Para realizar el
+    // movimiento se llama al método Move(), con la nueva dirección como
+    // parámetro.
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal");
@@ -15,6 +22,7 @@ public class Character : MonoBehaviour
         Move(movement);
     }
 
+    // Método que actualiza la posición del personaje.
     void Move(Vector3 direction)
     {
         transform.position += direction * speed * Time.deltaTime;
